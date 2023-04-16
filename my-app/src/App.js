d281c7f3aca3fb9ec340eddbb4fdc9a8661e336b
sayantan goswami <65398599+gsayant007@users.noreply.github.com>
2023-04-16 17:54:12 +0000

    commit #3 to commit the first react project/test.

---
+++ my-app/src/App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Gallery from './Gallery';
+
+// function Profile() {
+//   return (
+//     <img
+//       src="https://i.imgur.com/MK3eW3As.jpg"
+//       alt="Katherine Johnson"
+//     />
+//   );
+// }
+
+// export default function Gallery() {
+//   return (
+//     <section>
+//       <h1>Amazing scientists</h1>
+//       <Profile />
+//       <Profile />
+//       <Profile />
+//     </section>
+//   );
+// }
+
+export default function App() {
+  return (
+    <div><Gallery /></div>
+    
+  );
+}
---
+++ my-app/src/Gallery.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+function Profile1(){
+    return (
+      <img
+        src="https://i.imgur.com/QIrZWGIs.jpg"
+        alt="Alan L. Hart"
+      />
+    );
+  }
+
+
+function Profile2(){
+    return(
+        <img src="https://i.imgur.com/MK3eW3As.jpg"
+        alt="Katherine Johnson"
+        ></img>
+    );
+}
+  
+export default function Gallery() {
+    return (
+      <section>
+        <h1>Amazing scientists</h1>
+        <Profile1 />
+        <Profile2 />
+      </section>
+    );
+  }
+  
